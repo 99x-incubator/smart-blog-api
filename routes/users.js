3ad3f6d05c57c2bbc6126c4b48fe5acb45a58387
azkarmoulana <azkar.moulana@gmail.com>
2018-10-06 03:53:47 +0530

    'getkey'

--- routes/users.js
+++ routes/users.js
@@ -6,24 +6,35 @@ var User = require('../models/user');
 
 router.post('/signup', function(req, res, next){
 
-    var user = new User({
-      firstName: req.body.fname,
-      lastName: req.body.lname,
-      email: req.body.email,
-      password: req.body.password,
-      publicKey: stellerPublicKey,
-      creation_dt: Date.now()
-    });
+    let accPromise = stellar.createAccount();
+
+    accPromise.then(function(key){
+
+      var user = new User({
+        firstName: req.body.fname,
+        lastName: req.body.lname,
+        email: req.body.email,
+        password: req.body.password,
+        publicKey: key.publickey,
+        creation_dt: Date.now()
+      });
+    
+      let promise = user.save();
+    
+      promise.then(function(doc){
+        return res.status(201).json(doc);
+      })
   
-    let promise = user.save();
-  
-    promise.then(function(doc){
-      return res.status(201).json(doc);
+      promise.catch(function(err){
+        return res.status(501).json({message: 'Error registering user.'})
+      })
+      
+    })
+    promise.catch(function(err){
+      console.log("error in getting keys from steller");
     })
 
-    promise.catch(function(err){
-      return res.status(501).json({message: 'Error registering user.'})
-    })
+    
   
   })
 
