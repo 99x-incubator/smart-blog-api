c7af1f5dc6e2d0bccc8227e69fcf015dd784a218
azkarmoulana <azkar.moulana@gmail.com>
2018-10-06 06:52:13 +0530

    async_function

--- routes/users.js
+++ routes/users.js
@@ -3,43 +3,30 @@ var stellar = require('../controllers/stellar/createAccount');
 var stellarBalance = require('../controllers/stellar/getBalance');
 var router = express.Router();
 var User = require('../models/user');
+var createAccount = require('../controllers/stellar/createAccount');
 
 router.post('/signup', function(req, res, next){
-
-    // let accPromise = stellar.createAccount();
-
-    // accPromise.then(function(key){
-
-      var user = new User({
-        firstName: req.body.fname,
-        lastName: req.body.lname,
-        email: req.body.email,
-        password: req.body.password,
-        publicKey: "xnsd48383bnsnsmms92828tdbvsz",
-        creation_dt: Date.now()
-      });
-    
-      let promise = user.save();
-    
-      promise.then(function(doc){
-        return res.status(201).json(doc);
-      })
   
-      promise.catch(function(err){
-        return res.status(501).json({message: 'Error registering user.'})
-      })
-      
-    // })
-    // promise.catch(function(err){
-    //   console.log("error in getting keys from steller");
-    // })
+  var user = new User({
+    firstName: req.body.fname,
+    lastName: req.body.lname,
+    email: req.body.email,
+    password: req.body.password,
+    publicKey: createAccount.createAccount().publicKey,
+    creation_dt: Date.now()
+  });
 
-    
-  
+  let promise = user.save();
+
+  promise.then(function(doc){
+    return res.status(201).json(doc);
   })
 
+  promise.catch(function(err){
+    return res.status(501).json({message: 'Error registering user.'})
+  })
 
-
+});
 
 router.get('/createacc', stellar.createAccount);
 router.get('/balance/:publicKey', stellarBalance.getBalance);
