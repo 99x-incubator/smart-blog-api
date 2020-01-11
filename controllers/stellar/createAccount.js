34be63df54627cc86bb0fd217b6746c0d0dee0c7
ThanoshanMV <mvthanoshan9@gmail.com>
2020-01-11 21:49:09 +0530

    Deleting unnecessary comments

--- controllers/stellar/createAccount.js
+++ controllers/stellar/createAccount.js
@@ -1,78 +1,86 @@
-var StellarSdk = require('stellar-sdk');
+var StellarSdk = require("stellar-sdk");
 StellarSdk.Network.useTestNetwork();
-var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+var server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
 
-exports.createAccount = function(req, res, next){
+exports.createAccount = function(req, res, next) {
+  var pair = StellarSdk.Keypair.random();
 
-    var pair = StellarSdk.Keypair.random();
-    
-    pair.secret();
-    console.log(pair.secret());
-    pair.publicKey();
-    var request = require('request');
-    request.get({
-        url: 'https://friendbot.stellar.org',
-        qs: { addr: pair.publicKey() },
-        json: true
-    }, function (error, response, body) {
-        if (error || response.statusCode !== 200) {
-            console.error('ERROR!', error || body);
-        }
-        else {
-            console.log('SUCCESS! You have a new account :)\n', body);
-            // the JS SDK uses promises for most actions, such as retrieving an account
-            server.loadAccount(pair.publicKey()).then(function (account) {
-                console.log('Balances for account: ' + pair.publicKey());
-                account.balances.forEach(function (balance) {
-                    console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
-                });
-            });
-            // Keys for accounts to issue and receive the new asset
-            var issuingKeys = StellarSdk.Keypair
-                .fromSecret('SCKL62GED46Z6ZXN7IBPQX43KNSQOI62XD7DKNW4FDFYZQUPQZRTHFBV');
-            var receivingKeys = pair;
-            // var receivingKeys = StellarSdk.Keypair
-            //   .fromSecret('SDNW4TVMRPTO7NPSBYHYA2ESHOWQLQLOVDN3AKW6Q65YOVT7DIC3KYPO');
-            // Create an object to represent the new asset
-            var Blog = new StellarSdk.Asset('Blog', issuingKeys.publicKey());
-            // First, the receiving account must trust the asset
-            server.loadAccount(receivingKeys.publicKey())
-                .then(function (receiver) {
-                    var transaction = new StellarSdk.TransactionBuilder(receiver)
-                        // The `changeTrust` operation creates (or alters) a trustline
-                        // The `limit` parameter below is optional
-                        .addOperation(StellarSdk.Operation.changeTrust({
-                            asset: Blog,
-                            limit: '1000'
-                        }))
-                        .build();
-                    transaction.sign(receivingKeys);
-                    return server.submitTransaction(transaction);
+  pair.secret();
+  console.log(pair.secret());
+  pair.publicKey();
+  var request = require("request");
+  request.get(
+    {
+      url: "https://friendbot.stellar.org",
+      qs: { addr: pair.publicKey() },
+      json: true
+    },
+    function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        console.error("ERROR!", error || body);
+      } else {
+        console.log("SUCCESS! You have a new account :)\n", body);
+        // the JS SDK uses promises for most actions, such as retrieving an account
+        server.loadAccount(pair.publicKey()).then(function(account) {
+          console.log("Balances for account: " + pair.publicKey());
+          account.balances.forEach(function(balance) {
+            console.log(
+              "Type:",
+              balance.asset_type,
+              ", Balance:",
+              balance.balance
+            );
+          });
+        });
+        // Keys for accounts to issue and receive the new asset
+        var issuingKeys = StellarSdk.Keypair.fromSecret(
+          "SCKL62GED46Z6ZXN7IBPQX43KNSQOI62XD7DKNW4FDFYZQUPQZRTHFBV"
+        );
+        var receivingKeys = pair;
+        // Create an object to represent the new asset
+        var Blog = new StellarSdk.Asset("Blog", issuingKeys.publicKey());
+        // First, the receiving account must trust the asset
+        server
+          .loadAccount(receivingKeys.publicKey())
+          .then(function(receiver) {
+            var transaction = new StellarSdk.TransactionBuilder(receiver)
+              // The `changeTrust` operation creates (or alters) a trustline
+              // The `limit` parameter below is optional
+              .addOperation(
+                StellarSdk.Operation.changeTrust({
+                  asset: Blog,
+                  limit: "1000"
                 })
-                // Second, the issuing account actually sends a payment using the asset
-                .then(function () {
-                    return server.loadAccount(issuingKeys.publicKey());
+              )
+              .build();
+            transaction.sign(receivingKeys);
+            return server.submitTransaction(transaction);
+          })
+          // Second, the issuing account actually sends a payment using the asset
+          .then(function() {
+            return server.loadAccount(issuingKeys.publicKey());
+          })
+          .then(function(issuer) {
+            var transaction = new StellarSdk.TransactionBuilder(issuer)
+              .addOperation(
+                StellarSdk.Operation.payment({
+                  destination: receivingKeys.publicKey(),
+                  asset: Blog,
+                  amount: "20"
                 })
-                .then(function (issuer) {
-                    var transaction = new StellarSdk.TransactionBuilder(issuer)
-                        .addOperation(StellarSdk.Operation.payment({
-                            destination: receivingKeys.publicKey(),
-                            asset: Blog,
-                            amount: '20'
-                        }))
-                        .build();
-                    transaction.sign(issuingKeys);
-                    return server.submitTransaction(transaction);
-                })
-                .catch(function (error) {
-                    console.error('Error!', error);
-                });
-        }
-    });
-    return {
-        "secretKey" : pair.secret(),
-        "publicKey" : pair.publicKey(),
-    };
-
-}
-
+              )
+              .build();
+            transaction.sign(issuingKeys);
+            return server.submitTransaction(transaction);
+          })
+          .catch(function(error) {
+            console.error("Error!", error);
+          });
+      }
+    }
+  );
+  return {
+    secretKey: pair.secret(),
+    publicKey: pair.publicKey()
+  };
+};
--- models/user.js
+++ models/user.js
@@ -1,22 +1,11 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-// var bcrypt = require('bcrypt');
-
 var schema = new Schema({
-    email : {type:String, require:true},
-    firstName: {type:String, require:true},
-    lastName: {type:String, require:true},
-    password:{type:String, require:true},
-    publicKey:{type:String, require:false},
-    creation_dt:{type:Date, require:true}
+  email: { type: String, require: true },
+  firstName: { type: String, require: true },
+  lastName: { type: String, require: true },
+  password: { type: String, require: true },
+  publicKey: { type: String, require: false },
+  creation_dt: { type: Date, require: true }
 });
-
-// schema.statics.hashPassword = function hashPassword(password){
-//     return bcrypt.hashSync(password,10);
-// }
-
-// schema.methods.isValid = function(hashedpassword){
-//     return  bcrypt.compareSync(hashedpassword, this.password);
-// }
-
-module.exports = mongoose.model('User',schema)
+module.exports = mongoose.model("User", schema);
