c6d0a95adcf74382e2e47444e6943403c8a6f71f
ThanoshanMV <mvthanoshan9@gmail.com>
2020-02-01 16:30:39 +0530

    Removing unnecessary comments

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-//mongoose
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://azkar:azkar123@ds223653.mlab.com:23653/smartblog');
 
-//add cors
+
 var cors = require('cors');
 app.use(cors({
   origin:'http://localhost:4200'
 }));
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -32,18 +32,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+
   res.status(err.status || 500);
   res.render('error');
 });
--- controllers/stellar/createAccount.js
+++ controllers/stellar/createAccount.js
@@ -20,27 +20,19 @@ exports.createAccount = function(req, res, next){
         }
         else {
             console.log('SUCCESS! You have a new account :)\n', body);
-            // the JS SDK uses promises for most actions, such as retrieving an account
             server.loadAccount(pair.publicKey()).then(function (account) {
                 console.log('Balances for account: ' + pair.publicKey());
                 account.balances.forEach(function (balance) {
                     console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
                 });
             });
-            // Keys for accounts to issue and receive the new asset
             var issuingKeys = StellarSdk.Keypair
                 .fromSecret('SCKL62GED46Z6ZXN7IBPQX43KNSQOI62XD7DKNW4FDFYZQUPQZRTHFBV');
             var receivingKeys = pair;
-            // var receivingKeys = StellarSdk.Keypair
-            //   .fromSecret('SDNW4TVMRPTO7NPSBYHYA2ESHOWQLQLOVDN3AKW6Q65YOVT7DIC3KYPO');
-            // Create an object to represent the new asset
             var Blog = new StellarSdk.Asset('Blog', issuingKeys.publicKey());
-            // First, the receiving account must trust the asset
             server.loadAccount(receivingKeys.publicKey())
                 .then(function (receiver) {
                     var transaction = new StellarSdk.TransactionBuilder(receiver)
-                        // The `changeTrust` operation creates (or alters) a trustline
-                        // The `limit` parameter below is optional
                         .addOperation(StellarSdk.Operation.changeTrust({
                             asset: Blog,
                             limit: '1000'
@@ -49,7 +41,6 @@ exports.createAccount = function(req, res, next){
                     transaction.sign(receivingKeys);
                     return server.submitTransaction(transaction);
                 })
-                // Second, the issuing account actually sends a payment using the asset
                 .then(function () {
                     return server.loadAccount(issuingKeys.publicKey());
                 })
