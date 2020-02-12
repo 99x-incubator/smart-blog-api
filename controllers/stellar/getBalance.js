0d35f3dd34598587ed23926e07deb5d191966124
ThanoshanMV <mvthanoshan9@gmail.com>
2020-02-12 20:29:54 +0530

    Revert "Deleting unnecessary comments"
    This reverts commit 34be63df54627cc86bb0fd217b6746c0d0dee0c7.

--- controllers/stellar/getBalance.js
+++ controllers/stellar/getBalance.js
@@ -1,25 +1,30 @@
-var StellarSdk = require("stellar-sdk");
+var StellarSdk = require('stellar-sdk');
 StellarSdk.Network.useTestNetwork();
-var server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
-exports.getBalance = function(req, res, next) {
-  var publicKey = req.params.publicKey;
+var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
 
-  var server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
-  var arr = [];
-  // the JS SDK uses promises for most actions, such as retrieving an account
-  server.loadAccount(publicKey).then(function(account) {
-    console.log("Balances for account: " + publicKey);
-    account.balances.forEach(function(balance) {
-      console.log("Type:", balance.asset_type, ", Balance:", balance.balance);
-      arr.push({
-        Type: balance.asset_type,
-        Balance: balance.balance
-      });
+exports.getBalance = function (req, res, next) {
+    var publicKey = req.params.publicKey
+
+    var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+
+    var arr=[];
+    // the JS SDK uses promises for most actions, such as retrieving an account
+    server.loadAccount(publicKey).then(function (account) {
+        console.log('Balances for account: ' + publicKey);
+        account.balances.forEach(function (balance) {
+            console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
+            arr.push({
+                Type: balance.asset_type,
+                Balance: balance.balance
+            });
+        });
+        console.log(arr)
+        res.send({
+        "publicKey": publicKey,
+        "assets": arr
     });
-    console.log(arr);
-    res.send({
-      publicKey: publicKey,
-      assets: arr
     });
-  });
-};
+
+    
+}
+
